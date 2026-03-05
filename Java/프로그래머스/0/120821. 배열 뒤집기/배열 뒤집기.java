de3d8559e208e30e0133dfb57e5a9cc36154a678
ycbyun <chan200399@gmail.com>
2026-03-05 10:15:01 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.04 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] result = new int[num_list.length];
+        
+        for(int i = 0; i < num_list.length; i++){
+            result[i] = num_list[num_list.length - i - 1];
+        }
+        
+        return result;
+    }
+}
