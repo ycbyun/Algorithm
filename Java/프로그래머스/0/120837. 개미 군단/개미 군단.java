64bed96d7f2700dd88a063741578e779b1afa9bf
ycbyun <chan200399@gmail.com>
2026-03-25 10:51:04 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int hp) {
+        int result = 0;
+        result += hp / 5;
+        hp %= 5;
+        result += hp / 3;
+        hp %= 3;
+        result += hp;
+        return result;
+    }
+}
