3fcdcd9ece9355284f20d8830376431dd7076e93
ycbyun <chan200399@gmail.com>
2026-05-04 10:28:22 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.84 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+        return sb.toString();
+    }
+}
