dbe9fd38247867e4e5ec35dc919c9cb8e206d8a0
ycbyun <chan200399@gmail.com>
2026-06-10 11:00:18 +0900

    [level 0] Title: 다항식 더하기, Time: 0.16 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String solution(String polynomial) {
+        String[] terms = polynomial.split(" \\+ ");
+        int xCoefficient = 0;
+        int constant = 0;
+
+        for (String term : terms) {
+            if (term.contains("x")) {
+                if (term.equals("x")) {
+                    xCoefficient += 1;
+                } else {
+                    xCoefficient += Integer.parseInt(term.replace("x", ""));
+                }
+            } else {
+                constant += Integer.parseInt(term);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        if (xCoefficient > 0) {
+            if (xCoefficient == 1) {
+                result.append("x");
+            } else {
+                result.append(xCoefficient).append("x");
+            }
+        }
+
+        if (constant > 0) {
+            if (result.length() > 0) {
+                result.append(" + ").append(constant);
+            } else {
+                result.append(constant);
+            }
+        }
+
+        return result.toString();
+    }
+}
